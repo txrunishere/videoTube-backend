452a24aa468d254459ffbb09f19d0c1b8dd6d189
txrunishere <tsoni9286@gmail.com>
2025-03-12 22:48:27 +0530

    add tweet model with schema definitions and validation

---
+++ src/models/tweet.model.js
@@ -0,0 +1,19 @@
+import { Schema, model } from "mongoose"
+
+const tweetSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User"
+    }
+  },
+  { timestamps: true }
+);
+
+const Tweet = model("Tweet", tweetSchema);
+
+export { Tweet }
