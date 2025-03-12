fc810c1c940eadd595c85039dc30a87c2544ac05
txrunishere <tsoni9286@gmail.com>
2025-03-12 22:45:39 +0530

    add pagination support to video model using mongoose-aggregate-paginate

--- src/models/video.models.js
+++ src/models/video.models.js
@@ -1,4 +1,5 @@
 import { Schema, model } from "mongoose"
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema(
   {
@@ -41,6 +42,8 @@ const videoSchema = new Schema(
   },
   { timestamps: true }
 )
+
+videoSchema.plugin(mongooseAggregatePaginate)
 
 const Video = model("Video", videoSchema);
 export { Video }
