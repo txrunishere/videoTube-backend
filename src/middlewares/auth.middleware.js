bdea2ed36f259dea43f842519dbb14b0bc45d990
txrunishere <tsoni9286@gmail.com>
2025-03-14 13:02:23 +0530

    add JWT verification middleware for user authentication

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+import expressAsyncHandler from "express-async-handler";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.models.js";
+import jwt from "jsonwebtoken";
+
+const verifyJWT = expressAsyncHandler(async (req, _, next) => {
+  try {
+    const token = req.cookies?.accessToken;
+    if (!token) {
+      throw new ApiError(400, "Token not found!");
+    }
+  
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+  
+    console.log(decodedToken);
+    
+    const user = await User.findById(decodedToken._id).select("-password -refreshToken");
+    if (!user)
+      throw new ApiError(400, "User Not found!!")
+
+    req.user = user;
+
+    next()
+  } catch (error) {
+    throw new ApiError(401, error.message || "Something went wrong while check access token")
+  }
+});
+
+export { verifyJWT };
